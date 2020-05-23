cef83bbb27a8b63cb29d0d2d7b64493d3f27d2a2
mari <mari.17ymy@gmail.com>
2020-05-23 16:59:54 -0400

    GET, POST

--- index.php
+++ index.php
@@ -1,8 +1,22 @@
 <?php
 
-require_once("db.php");
+/*require_once("db.php");
 $query = "select * from persona";
 $resultado = ObtenerRegistros($query);
-print_r($resultado);
+print_r($resultado);*/
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' ) {
+    print('GET');
+    http_response_code(200);
+}else if ($_SERVER['REQUEST_METHOD'] == 'POST' ){
+    print('POST');
+    http_response_code(200);
+}else{
+    print('no hay respuesta');
+    http_response_code(400);
+}
+
+
 
 ?>
